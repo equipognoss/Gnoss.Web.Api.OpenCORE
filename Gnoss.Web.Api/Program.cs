1d46a0ab716325c2bbcd6e65f39663c1c9c9e1b3
Javier-RSP <javierruiz@gnoss.com>
2026-09-17 16:14:05 +0200

    Security Improvement

--- Gnoss.Web.Api/Program.cs
+++ Gnoss.Web.Api/Program.cs
@@ -1,4 +1,5 @@
 using Es.Riam.Gnoss.Util.General;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
